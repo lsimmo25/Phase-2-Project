df8bfd9680f8ece1bc02119eafa2630d6d5e6d4b
Lyle Simmons <lylesimmons125@gmail.com>
2024-03-31 12:51:23 -0400

    added and centered home page content

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,16 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Home() {
 
 
 
     return(
-        <div>
+        <div style={{textAlign: "center"}}>
             <header>
-                <h1></h1>
+                <h1>Dealership Statistics</h1>
             </header>
+            <section>
+                <h2>Total Vehicles: {/*display total number of vehicles here*/}</h2>
+            </section>
+            <section>
+                <h2>Active Customers: {/*display total number of active customers here*/}</h2>
+            </section>
         </div>
     )
 }
 
 export default Home
+
+
