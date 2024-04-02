5d633c8acdde6597912d3ba2e97003d8577488af
Lyle Simmons <lylesimmons125@gmail.com>
2024-04-02 14:27:26 -0400

    creating sort button for employees

--- src/Pages/EmployeesPage.js
+++ src/Pages/EmployeesPage.js
@@ -5,12 +5,17 @@ import AddEmployeeForm from "../components/AddEmployeeForm";
 
 function EmployeesPage({ employees, setEmployees, searchEmployees, setSearchEmployees}) {
 
+    const [showActive, setShowActive] = useState(false)
+
+    const filteredEmployees = showActive ? employees.filter(employee => employee.active) : employees
+
     return (
         <div>
             <h1>Employees</h1>
             <AddEmployeeForm setEmployees={setEmployees} employees={employees} />
             <section>
                 <EmployeeSearch employees={employees} setSearchEmployees={setSearchEmployees} searchEmployees={searchEmployees} />
+                <button></button>
             </section>
             <section>
                 <EmployeeList employees={employees} setEmployees={setEmployees} />
